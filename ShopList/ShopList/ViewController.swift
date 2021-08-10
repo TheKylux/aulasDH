32a082a568f37394af6605539c1e4e8af371367d
Vitor Lentos <lentos.exe@gmail.com>
2021-08-10 19:51:47 -0300

    1.0
    Criação do projeto ShopList

---
+++ ShopList/ShopList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShopList
+//
+//  Created by Vitor Lentos on 10/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
